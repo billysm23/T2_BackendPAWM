e6f62b86085302362bda7d99998aabbd64d0bad1
billysm23 <bllysm23@gmail.com>
2024-11-17 07:43:28 +0700

    tes schema

--- src/models/lesson.js
+++ src/models/lesson.js
@@ -1,25 +1,93 @@
 const mongoose = require('mongoose');
 
+const additionalReadingSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  url: String,
+  type: String
+}, { _id: false });
+
 const lessonSchema = new mongoose.Schema({
-    title: {
-      type: String,
-      required: true,
-      unique: true
-    },
-    order: {
-      type: Number,
-      required: true
-    },
+  _id: String,
+  order: Number,
+  title: {
+    type: String,
+    required: true
+  },
+  description: String,
+  duration: String,
+  practiceTime: String,
+  level: String,
+  sections: Number,
+  prerequisite: [{
+    type: String,
+    ref: 'Lesson'
+  }],
+
+  // Overview Section
+  learningObjectives: [String],
+  prerequisites: [String],
+  topics: [{
+    title: String,
     description: String,
-    content: {
-      type: String,
-      required: true
-    },
-    prerequisites: [{
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Lesson'
-    }]
-  });
+    icon: String
+  }],
+
+  // Content Section
+  content: String,
+  keyConcepts: [{
+    title: String,
+    description: String,
+    example: String
+  }],
+
+  practiceProblems: [{
+    title: String,
+    description: String,
+    hint: String
+  }],
+
+  // Resources Section
+  additionalReading: [additionalReadingSchema],
   
-const Lesson = mongoose.model('Lesson', lessonSchema);  
+  videos: [{
+    title: String,
+    url: String,
+    duration: String,
+    thumbnail: String
+  }],
+
+  documents: [{
+    title: String,
+    description: String,
+    url: String,
+    size: String
+  }],
+
+  externalLinks: [{
+    title: String,
+    description: String,
+    url: String
+  }],
+
+  // Quiz Section
+  quiz: [{
+    _id: String,
+    question_text: String,
+    type: {
+      type: String,
+      enum: ['multiple_choice', 'true_false']
+    },
+    options: [{
+      _id: String,
+      text: String,
+      isCorrect: Boolean
+    }],
+    order: Number
+  }]
+}, {
+  timestamps: true
+});
+
+const Lesson = mongoose.model('Lesson', lessonSchema);
 module.exports = Lesson;
